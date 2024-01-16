93722a0707177615a653e465679a7fa0ccb0942a
shaun <shaun.17g@gmail.com>
2024-01-16 14:58:06 +0800

    style: add init.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qweather-api',
-    version='0.1.0',
+    version='0.1.2',
     author='wenren',
     author_email='shaun.17g@gmail.com',
     packages=find_packages(),
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from model import (WeatherData, WeatherSun, WeatherMoon, WeatherAirData, WeatherAirNow, WeatherIndicesData)
+from qweather import QWeatherPy
+from http_request_manager import HttpRequestsManager
